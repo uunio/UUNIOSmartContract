d4d0f01b42a884fe3c580657098198013ff8dcbf
moon <moon@moons-MacBook-Pro.local>
2018-05-09 11:34:05 +0900

    bug fix and running testnet

--- UUNIOToken.sol
+++ UUNIOToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.16;    
 // pragma solidity ^0.4.13;
 
 library Math {
@@ -36,18 +36,12 @@ library Math {
 }
 
 
-contract QRC20Basic {
-
+contract QRC20 {
 
     function totalSupply() public view returns (uint256);
     function balanceOf(address who) public view returns (uint256);
     function transfer(address to, uint256 value) public returns (bool);
     event Transfer(address indexed from, address indexed to, uint256 value);
-}
-
-
-contract QRC20 is QRC20Basic {
-
 
     function allowance(address owner, address spender) public view returns (uint256);
     function transferFrom(address from, address to, uint256 value) public returns (bool);
@@ -62,7 +56,7 @@ contract Ownable {
     address public owner;
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    function Ownable() public {
+    constructor() public {
         
         owner = msg.sender;
     }
@@ -76,19 +70,19 @@ contract Ownable {
     function transferOwnership(address newOwner) public onlyOwner {
         
         require(newOwner != address(0));
-        OwnershipTransferred(owner, newOwner);
+        emit OwnershipTransferred(owner, newOwner);
         owner = newOwner;
     }
 }
 
 
-contract BasicToken is QRC20Basic {
+contract BasicToken is QRC20 {
     
 
-    using SafeMath for uint256;
+    using Math for uint256;
     
     uint256 totalSupply_;    
-    mapping(address => uint256) balances;
+    mapping(address => uint256) balances_;
 
     function totalSupply() public view returns (uint256) {
         
@@ -98,17 +92,17 @@ contract BasicToken is QRC20Basic {
     function transfer(address _to, uint256 _value) public returns (bool) {
 
         require(_to != address(0));
-        require(_value <= balances[msg.sender]);
+        require(_value <= balances_[msg.sender]);
 
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        Transfer(msg.sender, _to, _value);
+        balances_[msg.sender] = balances_[msg.sender].sub(_value);
+        balances_[_to] = balances_[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
     function balanceOf(address _owner) public view returns (uint256 balance) {
 
-        return balances[_owner];
+        return balances_[_owner];
     }
 }
 
@@ -119,11 +113,11 @@ contract BurnableToken is BasicToken {
 
     function burn(uint256 _value) public {
 
-        require(_value <= balances[msg.sender]);
+        require(_value <= balances_[msg.sender]);
         address burner = msg.sender;
-        balances[burner] = balances[burner].sub(_value);
+        balances_[burner] = balances_[burner].sub(_value);
         totalSupply_ = totalSupply_.sub(_value);
-        Burn(burner, _value);
+        emit Burn(burner, _value);
     }
 }
 
@@ -133,23 +127,23 @@ contract StandardToken is QRC20, BasicToken {
 
     mapping (address => mapping (address => uint256)) internal allowed;
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    function transferFrom(address _from, address _to, uint256 _value) public returns const (bool) {
 
         require(_to != address(0));
-        require(_value <= balances[_from]);
+        require(_value <= balances_[_from]);
         require(_value <= allowed[_from][msg.sender]);
 
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
+        balances_[_from] = balances_[_from].sub(_value);
+        balances_[_to] = balances_[_to].add(_value);
         allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-        Transfer(_from, _to, _value);
+        emit Transfer(_from, _to, _value);
         return true;
     }
 
     function approve(address _spender, uint256 _value) public returns (bool) {
         
         allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
@@ -157,39 +151,23 @@ contract StandardToken is QRC20, BasicToken {
         
         return allowed[_owner][_spender];
     }
-
-    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
-        
-        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        return true;
-    }
-
-    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
-        
-        uint oldValue = allowed[msg.sender][_spender];
-        if (_subtractedValue > oldValue) {
-            allowed[msg.sender][_spender] = 0;
-        } else {
-            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-        }
-        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        return true;
-    }
 }
 
 
-contract UUNIOToken is StandardToken, BurnableToken, Ownable {
+contract UUNIO is StandardToken, BurnableToken, Ownable {
 
     
-    using SafeMath for uint;
+    using Math for uint;
 
     string constant public symbol  = "UUNIO";
     string constant public name    = "UUNIO";
     uint8 constant public decimals = 8;
 
-    uint256 const INITIAL_SUPPLY   = 9e8;
+    uint256 constant INITIAL_SUPPLY   = 900000000e8;
 
+
+
+    // MAINNET
     address team      = 0x9c619ff74015becc48d429755aa54435ba367e23;
     address advisors  = 0xb4fca416727c92f5dbfc1d3c248f9a50b9f811fe;
     address reserve   = 0x8e2c648f493323623c2a55010953ae2b98ec7675;
@@ -198,25 +176,37 @@ contract UUNIOToken is StandardToken, BurnableToken, Ownable {
     address partners  = 0x8f3e215c76b312fd28fbaaf16fe98d6e9357b8ab;
     address preSale   = 0x39401cd3f45c682bbb75ea4d3add4e268b19d0fc;
     address crowdSale = 0xb06dd470c23979f8331e790d47866130001e7492;
+    address benefit   = 0x0ff19b60b84040019ea6b46e6314367484f66f8f;
+    
+    // TESTNET
+    // address team        = 0x08cF66b63c2995c7Cc611f58c3Df1305a1E46ba7;
+    // address advisors    = 0xCf456ED49752F0376aFd6d8Ed2CC6e959E57C086;
+    // address reserve     = 0x9F1046F1e85640256E2303AC807F895C5c0b862b;
+    // address system      = 0xC97eFe0481964b344Df74e8Fa09b194010736A62;
+    // address angel       = 0xd03631463a266A749C666E6066D835bDAD307FB8;
+    // address partners    = 0xd03631463a266A749C666E6066D835bDAD307FB8;
+    // address preSale     = 0xd03631463a266A749C666E6066D835bDAD307FB8;
+    // address crowdSale   = 0xd03631463a266A749C666E6066D835bDAD307FB8;
+    // address beneficiary = 0x08cF66b63c2995c7Cc611f58c3Df1305a1E46ba7;
 
     // 10%
-    uint constant teamTokens      = 0.9e8;
+    uint constant teamTokens      = 90000000e8;
     // 10%    
-    uint constant advisorsTokens  = 0.9e8;
+    uint constant advisorsTokens  = 90000000e8;
     // 30%    
-    uint constant reserveTokens   = 2.7e8;
+    uint constant reserveTokens   = 270000000e8;
     // 15.14%
-    uint constant systemTokens    = 1.3626e8;
+    uint constant systemTokens    = 130000000e8;
     // 5.556684%
-    uint constant angelTokens     = 0.50010156e8;
+    uint constant angelTokens     = 50010156e8;
     // 2.360022%
-    uint constant partnersTokens  = 0.21240198e8;
+    uint constant partnersTokens  = 21240198e8;
     // 15.275652%
-    uint constant preSaleTokens   = 1.37480868e8;
+    uint constant preSaleTokens   = 137480868e8;
     // 11.667642%
-    uint constant crowdSaleTokens = 1.05008778e8;
+    uint constant crowdSaleTokens = 105008778e8;
 
-    function UUNIOToken() public {
+    constructor() public {
 
         totalSupply_ = INITIAL_SUPPLY;
 
@@ -231,8 +221,8 @@ contract UUNIOToken is StandardToken, BurnableToken, Ownable {
     }
 
     function preFixed(address _address, uint _amount) internal returns (bool) {
-        balances[_address] = _amount;
-        Transfer(address(0x0), _address, _amount);
+        balances_[_address] = _amount;
+        emit Transfer(address(0x0), _address, _amount);
         return true;
     }
 
@@ -247,8 +237,7 @@ contract UUNIOToken is StandardToken, BurnableToken, Ownable {
     }
 
     function () public payable {
-
         require(msg.value >= 1e8);
-        beneficiary.transfer(msg.value);
+        benefit.transfer(msg.value);
     }
 }
